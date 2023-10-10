19ae176fbbea6b20b1721a64d98ecb6381909b16
gmesmo <guimachado94@gmail.com>
2023-10-10 17:55:49 -0300

    Changes to be committed: 	modified:   src/components/easybooking/EasyBooking.module.css 	modified:   src/components/easybooking/EasyBooking.tsx

--- src/components/easybooking/EasyBooking.tsx
+++ src/components/easybooking/EasyBooking.tsx
@@ -5,27 +5,36 @@ import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import "@mui/material/styles";
+import "dayjs/locale/pt";
 
 import styles from "./EasyBooking.module.css";
+import dayjs, { Dayjs } from "dayjs";
 
 const EasyBooking: React.FC = () => {
   const [pessoas, setPessoas] = useState<number | undefined>(1);
-
   const [crianca, setCrianca] = useState<number | undefined>(0);
+  const [primeiraData, setPrimeiraData] = useState(dayjs());
 
   const handlePessoasChange = (novoValor: number | number[]) => {
     const novoValorNumber = Array.isArray(novoValor) ? novoValor[0] : novoValor;
     setPessoas(novoValorNumber);
-    if (crianca >= novoValorNumber - 1) {
+    if ((crianca || 0) >= novoValorNumber - 1) {
       setCrianca(novoValorNumber - 1);
     }
   };
 
   const handleCriancaChange = (novoValor: number | number[]) => {
     const novoValorNumber = Array.isArray(novoValor) ? novoValor[0] : novoValor;
+    const pessoasDefinido = pessoas ?? 0;
     const valor =
-      novoValorNumber <= pessoas - 1 ? novoValorNumber : pessoas - 1;
+      novoValorNumber <= pessoasDefinido - 1
+        ? novoValorNumber
+        : pessoasDefinido - 1;
     setCrianca(valor);
+  };
+
+  const handleFirstDateChange = (novoValor: Dayjs) => {
+    setPrimeiraData(dayjs(novoValor));
   };
 
   return (
@@ -57,13 +66,28 @@ const EasyBooking: React.FC = () => {
           onChange={(_, novoValor) => handleCriancaChange(novoValor)}
           valueLabelDisplay="on"
           valueLabelFormat={(value) => value.toString()}
+          sx={{
+            transition: "color 0.5s",
+            ...(pessoas === 1 ? { color: "red" } : {}),
+          }}
         />
       </div>
-      <p>Valor selecionado: {pessoas}</p>
 
-      <LocalizationProvider dateAdapter={AdapterDayjs}>
-        <DatePicker />
-      </LocalizationProvider>
+      <div className={styles.DatesWrapper}>
+        <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="pt">
+          <DatePicker
+            disablePast
+            label={"Primeiro dia"}
+            value={primeiraData}
+            onChange={(novoValor) =>
+              handleFirstDateChange(novoValor || dayjs())
+            }
+          />
+          <DatePicker disablePast label={"Último dia"} />
+        </LocalizationProvider>
+      </div>
+
+      <p>Valor selecionado: {pessoas}</p>
     </div>
   );
 };
