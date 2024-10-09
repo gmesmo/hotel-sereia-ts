621b72a58a32afe6ee3ff140a5a186f2094c5ea4
GMesmo <guimachado94@gmail.com>
2024-10-09 16:13:01 -0300

    ajuste

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,8 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
-
-const __dirname = new URL(".", import.meta.url).pathname;
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
   plugins: [react(), svgr()],
-
-  build: {
-    // lib: {
-    //   entry: "../../index.html", // Ajuste aqui se necessário
-    //   name: "core-package",
-    //   fileName: "index",
-    // },
-    rollupOptions: {
-      external: ["react", "react-router", "react-router-dom"],
-      output: {
-        globals: {
-          react: "React",
-        },
-      },
-    },
-  },
 });
